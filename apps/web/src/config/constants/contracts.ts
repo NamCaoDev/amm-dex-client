854174258815d953aa54ba1e74191314cf6f2f68
namcao2610 <namcaodev@gmail.com>
2023-03-13 15:08:56 +0700

    Feature: Upgrade lottery

--- apps/web/src/config/constants/contracts.ts
+++ apps/web/src/config/constants/contracts.ts
@@ -15,7 +15,7 @@ export default {
     56: '0x6ab8463a4185b80905e05a9ff80a2d6b714b9e95',
   },
   lotteryV2: {
-    97: '0x5790c3534F30437641541a0FA04C992799602998',
+    97: '0x31aE3B292d95449C91FF49736B2990126Ebeb869',
     56: '0x5aF6D33DE2ccEC94efb1bDF8f92Bd58085432d2c',
   },
   multiCall: multicallAddresses,
